9a8fa71034a0d6dc54f8a8a742991a7a24f9d4d6
ceslo <slovesce@gmail.com>
2023-11-16 15:37:59 +0100

    modif organistation des dossiers et selection des plats avec json

---
+++ assets/js/pages.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+  var x;
+  var y;
+
+  $.getJSON("assets/js/the_district.json", function (data) {
+    function affichage(x) {
+      $.each(data.plat, function (index,plats) {
+        if (x == plats.id_categorie) {
+          $("#aff_cat").append(
+            '<div class="card rounded col-12 col-lg-5"><div class="row"><div class="d-flex col-4 align-items-center"><img src="assets/images_the_district/food/' +
+      plats.image +
+      '"class="img-fluid rounded" alt="' +
+      plats.libelle +
+      '"/></div><div class="col-7 col-sm-8"><div class="card-body"><p class="card-title fs-4">' +
+      plats.libelle +
+      '</p><p class="card-text small">' +
+      plats.description +
+      '</p> <div class="d-flex justify-content-end my-3"><a class="btn btn-outline-dark" href="">Commander</a></div></div></div></div></div>'
+          );
+        }
+      });
+    }
+    function cherche(y){
+      if($("#recherche").val)
+   
+    }
+
+    $.each(data.categorie, function (index, cat) {
+      $("#aff_cat").append(
+        
+        '<button id="btn_' +
+          cat.libelle +
+          '" class="card rounded col-2 m-3" ><img class="card-img img-fluid" src="assets/images_the_district/category/' +
+          cat.image +
+          '" alt="Catégorie' +
+          (index + 1) +
+          '" title="' +
+          cat.libelle +
+          '"/> <div class="card-img-overlay d-flex align-items-center justify-content-center"> <h2 class="mark rounded text-center" style="color: #970747">' +
+          cat.libelle +
+          " </h2> </div> </button>"
+      );
+      $("#btn_" + cat.libelle).on("click", function () {
+        $("#aff_cat").empty();
+        var x = cat.id_categorie;
+        affichage(x)
+      });
+    });
+  });
+});
+
+// $("#aff_cat").remove();
+// console.log(data.categorie.id_categorie);
+// console.log(data.plat.id_categorie);
+
+// if (data.plat.id_categorie == data.categorie.id_categorie){
+//       console_log(data.plat.libelle)
+//      };
+
+// $.each(data.plat, function (index, plats) {
+//   $("#aff_plats").append(
+//     '<div class="card rounded col-12 col-lg-5"><div class="row"><div class="d-flex col-4 align-items-center"><img src="assets/images_the_district/food/' +
+//       plats.image +
+//       '"class="img-fluid rounded" alt="' +
+//       plats.libelle +
+//       '"/></div><div class="col-7 col-sm-8"><div class="card-body"><p class="card-title fs-4">' +
+//       plats.libelle +
+//       '</p><p class="card-text small">' +
+//       plats.description +
+//       '</p> <div class="d-flex justify-content-end my-3"><a class="btn btn-outline-dark" href="">Commander</a></div></div></div></div></div>'
+//   );
+// });
---
+++ assets/js/validation.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  function verif() {
+    var envoi = true;
+    var x = $("[id^=req_]");
+    var er_small= $("[id^=req_]+small")
+    console.log(x)
+    $(x).each(function(){
+      if (x.val() == "") {
+      envoi = false;
+      $(x).addClass("alert alert-danger")
+      $(er_small).html("Ce champ est obligatoire")
+      $(er_small).addClass("text-danger mt-0 pt-0 mb-3");
+     
+     }
+      else {envoi==true}
+    });
+
+    // if ($("#traitement_info").is(":not(:checked)")) {
+    //   envoi = false;
+    //   $("#label_info").addClass("text-danger");
+    // };
+    // if ($("#req_nom").val().match(":not(/^[A-Za-z]+$/)")) {
+    //   envoi = false;
+    //   $("#req_nom").addClass("alert alert-danger")
+    //   $(er_small).html("Le nom de doit comporter que des lettres")
+    //   $(er_small).addClass("text-danger mt-0 pt-0 mb-3")
+    //   ;
+    // };
+        
+    if (envoi == true) {
+      $("#contact").submit();
+    };
+  };
+
+ $("#envoyer").click(function (e) {
+    e.preventDefault();
+    verif();
+  });
+});
+
+
